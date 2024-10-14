a4a052a2afcdf223c054bb8cf76129ee7ac9a1ce
Cy <ogbangcyprian360@gmail.com>
2024-10-14 16:58:20 +0100

    Debugging for first deployment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material"
 import { Navbar, Feed, ChannelDetail, SearchFeed, VideoDetail, } from './Components'
 
+import "./Index.css";
 
 const App = () => (
   <BrowserRouter>
